4eb9893198b13dda7c08a1c605618c8bc15224d7
iraklyshatashvili <irakly1986@gmail.com>
2023-07-26 18:07:15 +0300

    add Lesson_1.py

--- Lesson_1.py
+++ Lesson_1.py
@@ -1,5 +1,31 @@
-value = 3
-new_value = 5
-my_str =  "Hello!!"
-print(value,new_value,my_str)
+# # value = 3
+# # new_value = 5
+# # my_str =  "Hello!!"
+# # print(value,new_value,my_str)
+#
+# val_1 = 21
+# val_2 = 4
+# # result = val_1 + val_2
+# # result = val_1 • val_2
+# # result = val_1 - val_2
+# # result = val_1 // val_2
+#
+#
+# print(result, type(result))
 
+# val_int = 3
+# # val_float = 4.3
+# # val_int = val_float
+# print(id(val_int))
+
+# value = "1 * 2 * 3"
+#
+# print(value)
+#
+# non_val = None
+#
+# print(non_val, type(non_val))
+
+val = None
+Id_val = id(val)
+print(Id_val)
